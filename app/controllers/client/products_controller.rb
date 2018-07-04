a9c5dabe9e4a3590a54b2d13eb63288c5a87a4be
coryrgriffith <corygriffith9@gmail.com>
2018-07-03 20:27:56 -0500

    make edit form work with update action

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -35,4 +35,16 @@ class Client::ProductsController < ApplicationController
     @product = response.body
     render "edit.html.erb"
   end
+
+  def update
+    client_params = {
+      input_name: params[:input_name],
+      input_price: params[:input_price],
+      input_image_url: params[:input_image_url],
+      input_description: params[:input_description]
+    }
+    response = Unirest.patch("localhost:3000/api/products/#{params[:id]}", parameters: client_params)
+    @product = response.body
+    render "show.html.erb"
+  end
 end
